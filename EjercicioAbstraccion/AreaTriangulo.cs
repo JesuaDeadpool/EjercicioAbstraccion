d7653446bd90b32f2d8428c122084b4e58b4ebc9
Jesua <144072667+JesuaDeadpool@users.noreply.github.com>
2025-02-07 23:13:20 -0600

    Creacion de la abstract class FormulaMatematica , creacion e implementacion de los atributos y de los metodos de la abstract class en AreaTriangulo, en el main creacion del menu ,switch de las opciones

---
+++ EjercicioAbstraccion/AreaTriangulo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioAbstraccion
+{
+    public class AreaTriangulo : FormulaMatematica
+    {
+
+        public double Base { get; set; }
+
+        public double Altura { get; set; }
+
+        public override double Calcular()
+        {
+            return 1 / 2 * (Base * Altura); 
+        }
+
+        public override void Imprimir()
+        {
+            Console.WriteLine(); 
+            Console.WriteLine($"Un triangulo con su Base {Base} y su altura {Altura}, Su Area es de : {Calcular().ToString("F2")}"); 
+            
+        }
+    }
+}
--- EjercicioAbstraccion/Program.cs
+++ EjercicioAbstraccion/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+using EjercicioAbstraccion;
+
+int opcion = 0;
+AreaTriangulo areaTriangulo = new AreaTriangulo();
+
+Console.WriteLine("-------OPERACIONES MATEMATICAS--------");
+Console.WriteLine("        1. Area de un Triangulo       ");
+Console.WriteLine("        2. Metros a Kilometros        ");
+Console.WriteLine("-------ELIJA UNA OPCION DEL 1 AL 3----");
+opcion = Convert.ToInt32(Console.ReadLine());
+
+switch (opcion)
+{
+    case 1:
+        Console.WriteLine("------Area de un Triangulo-----");
+        Console.WriteLine();
+        Console.WriteLine("Ingrese la base del triangulo : ");
+        areaTriangulo.Base = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese la altura del triangulo : ");
+        areaTriangulo.Altura = Convert.ToInt32(Console.ReadLine());
+
+        areaTriangulo.Imprimir(); 
+        
+        break;
+
+    case 2:
+        break;
+
+    case 3:
+
+        break;
+
+    default:
+
+        break; 
+}
